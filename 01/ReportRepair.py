41bebdf2f9d15678df220e693d59f39a0d5830c9
S1mon <s1monp@mailbox.org>
2020-12-03 16:59:50 +0100

    01 First Part

--- 01/ReportRepair.py
+++ 01/ReportRepair.py
@@ -1,1 +1,21 @@
-print("Hello World!");
+# checks if number one plus number two equals 2020
+def checkNumbers(firstNumber, secNumber):
+    if firstNumber + secNumber == 2020:
+        return True
+    return False
+
+
+numbers = open("input.txt")
+numberList = []
+for number in numbers:
+    numberList.append(int(number.rstrip()))
+
+finished = False
+
+# iterate through all different number combinations in the List
+for firstNumber in numberList:
+    for secNumber in numberList:
+        if checkNumbers(firstNumber, secNumber) and not finished:
+            print(f"{firstNumber} + {secNumber} = 2020")
+            print(f"{firstNumber} * {secNumber} = " + str(firstNumber * secNumber))
+            finished = True
